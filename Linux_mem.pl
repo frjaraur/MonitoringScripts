84f7a9067e2f2a4d3045b4e5e9e7adf3c6099097
Javier R <frjaraur@gmxcom>
2016-01-14 23:05:47 +0100

    First Public Commit for this repo

---
+++ Linux_mem.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+my %mem;
+open (F,'</proc/meminfo');
+while (<F>){
+	my ($var,$value)=split(' ',$_);
+	$var=~s/://g;
+	$mem{$var}=$value;
+}
+
+#foreach my $k (keys %mem){
+#	print "$k --> $mem{$k}\n";
+#}
+
+my $percentused=100*($mem{'MemTotal'}-($mem{'MemFree'}+$mem{'Buffers'}+$mem{'Cached'}))/$mem{'MemTotal'};
+
+printf ("%.1f \n",$percentused);
